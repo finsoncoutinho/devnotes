edc2badc6b2360c50be02c5551577683f266d658
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2023-12-29 09:43:03 +0530

    feat: created app using express and started listening on port on db connection success

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,15 @@
 import dotenv from 'dotenv'
+import { app } from './app'
 import connectDB from './db'
 
 dotenv.config()
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log('Database connection error :', err)
+  })
