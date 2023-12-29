12c2825efb5a1eae1505f61b0b9250eaa47deb07
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2023-12-30 00:15:59 +0530

    fix: updated port number

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -6,8 +6,8 @@ dotenv.config()
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port : ${process.env.PORT}`)
+    app.listen(process.env.PORT || 8001, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`)
     })
   })
   .catch((err) => {
