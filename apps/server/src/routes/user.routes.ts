5973ad998b7c466d9c58bc32b75be5e12338e0b9
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2024-01-01 16:24:17 +0530

    feat: created refresh token route

--- apps/server/src/routes/user.routes.ts
+++ apps/server/src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from '../controllers/user.controller'
 import { verifyJWT } from '../middlewares/auth.middleware'
@@ -14,5 +15,6 @@ router.route('/login').post(loginUser)
 
 // secured routes
 router.route('/logout').post(verifyJWT, logoutUser)
+router.route('/refresh-token').post(refreshAccessToken)
 
 export default router
