8f540295062da2adfaed131d18634ba2eea4efda
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2024-01-01 13:21:17 +0530

    feat: created login and logout routes

--- apps/server/src/routes/user.routes.ts
+++ apps/server/src/routes/user.routes.ts
@@ -1,8 +1,18 @@
 import { Router } from 'express'
-import { registerUser } from '../controllers/user.controller'
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from '../controllers/user.controller'
+import { verifyJWT } from '../middlewares/auth.middleware'
 
 const router = Router()
 
 router.route('/register').post(registerUser)
+
+router.route('/login').post(loginUser)
+
+// secured routes
+router.route('/logout').post(verifyJWT, logoutUser)
 
 export default router
