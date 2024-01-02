5ef9fa3ddf6a51a8e69e491286f2bb712b11bd5e
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2024-01-02 23:58:01 +0530

    fix: added export to uploadOnCloudinary

--- apps/server/src/utils/cloudinary.ts
+++ apps/server/src/utils/cloudinary.ts
@@ -7,7 +7,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-const uploadOnCloudinary = async (localFilePath: string) => {
+export const uploadOnCloudinary = async (localFilePath: string) => {
   try {
     if (!localFilePath) return null
 
