29f3a0eb30aafa7d2c821a16faa4348fb300d9b4
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2024-01-07 21:54:07 +0530

    feat: modified update details route and created change password, become seller, user details, verification email and verify user routes

--- apps/server/src/routes/user.routes.ts
+++ apps/server/src/routes/user.routes.ts
@@ -1,22 +1,34 @@
 import { Router } from 'express'
 import {
+  becomeSeller,
+  changeCurrentPassword,
+  getCurrentUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  sendVerificationEmail,
   updateUserDetails,
+  verifyUser,
 } from '../controllers/user.controller'
 import { verifyJWT } from '../middlewares/auth.middleware'
+import { upload } from '../middlewares/multer.middleware'
 
 const router = Router()
 
 router.route('/register').post(registerUser)
-
 router.route('/login').post(loginUser)
 
 // secured routes
 router.route('/logout').post(verifyJWT, logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
-router.route('/update-details').patch(verifyJWT, updateUserDetails)
+router
+  .route('/update-details')
+  .patch(verifyJWT, upload.single('avatar'), updateUserDetails)
+router.route('/change-password').patch(verifyJWT, changeCurrentPassword)
+router.route('/become-seller').patch(verifyJWT, becomeSeller)
+router.route('/details').get(verifyJWT, getCurrentUser)
+router.route('/verification-email').post(verifyJWT, sendVerificationEmail)
+router.route('/verify-user').patch(verifyUser)
 
 export default router
