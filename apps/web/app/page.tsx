47ea2a3f0fff44fd9a0793f856a4188a4674ad5a
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2024-01-11 21:46:05 +0530

    feat: setup tailwind css

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react'
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>page</div>
+  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
 }
 
 export default page
