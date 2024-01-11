fc6f2e8783d963b1f86dcba75ec98e59f9775638
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2024-01-11 22:15:14 +0530

    feat: setup shadcn ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,22 @@
+'use client'
+
 import { FC } from 'react'
+import { Button } from '@/components/ui/button'
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  const handleClick = () => {
+    console.log('clicked')
+  }
+  return (
+    <h1 className='text-3xl font-bold underline'>
+      Welcome to devnotes
+      <Button variant='default' onClick={handleClick}>
+        Sign up
+      </Button>
+    </h1>
+  )
 }
 
 export default page
