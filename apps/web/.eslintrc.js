a5edc0f5507bcd6e1832f81b1b26500e8cb02bcc
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2024-01-13 23:18:22 +0530

    feat: installed react query & its devtools & eslint plugin, react hook form and react hot toast

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,9 +1,18 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
+  extends: [
+    '@repo/eslint-config/next.js',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+  ],
+  plugins: ['@tanstack/query'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
-};
+  rules: {
+    '@tanstack/query/exhaustive-deps': 'error',
+    '@tanstack/query/no-rest-destructuring': 'warn',
+    '@tanstack/query/stable-query-client': 'error',
+  },
+}
