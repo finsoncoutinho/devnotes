08e6c44a2874c414eaea8a95f30c99c62746f142
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2024-01-15 00:46:01 +0530

    feat: created signup page

---
+++ apps/web/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from "./_components/SignUpForm";
+
+const page = () => {
+  return (
+    <div className=" flex h-screen items-center justify-center">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default page;
