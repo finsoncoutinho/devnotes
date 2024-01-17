e6f3659fcd2e4a764bf0757c3e72f0fb8aaa684e
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2024-01-17 22:48:41 +0530

    style: updated layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,44 +1,45 @@
-import ReactQueryProvider from '@/lib/ReactQueryProvider'
-import { Toaster } from 'react-hot-toast'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import ReactQueryProvider from "@/lib/ReactQueryProvider";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'DevNotes',
-  description: 'Marketplace for free and premium software engineering notes',
-}
+  title: "DevNotes",
+  description: "Marketplace for free and premium software engineering notes",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
         <ReactQueryProvider>
           {children}
+          {/* <div className="dark">{children}</div> */}
           <Toaster
-            position='top-center'
+            position="top-center"
             gutter={12}
-            containerStyle={{ margin: '8px' }}
+            containerStyle={{ margin: "8px" }}
             toastOptions={{
               success: { duration: 3000 },
               error: {
                 duration: 5000,
               },
               style: {
-                fontSize: '16px',
-                maxWidth: '500px',
-                padding: '16px 24px',
+                fontSize: "16px",
+                maxWidth: "500px",
+                padding: "16px 24px",
               },
             }}
           />
         </ReactQueryProvider>
       </body>
     </html>
-  )
+  );
 }
