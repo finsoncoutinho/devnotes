de3cf799532a8a79bf202e5b4f4b205e74786852
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2024-01-17 22:48:55 +0530

    feat: updated page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,15 @@
+import Header from "@/components/Header";
 import { FC } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <h1 className="text-3xl font-bold underline">Welcome to devnotes</h1>;
+  return (
+    <div>
+      <Header />
+      Welcome to devnotes
+    </div>
+  );
 };
 
 export default page;
